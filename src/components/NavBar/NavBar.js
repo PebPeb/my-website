b955d781739f972a72ff79b7efc1fd70dc9a6260
PebPeb <67885698+PebPeb@users.noreply.github.com>
2023-05-15 16:22:39 -0400

    Added Mobile Functionality
    Added mobile functionality to the navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
@@ -7,6 +7,7 @@ import "./NavBar.css";
 
 
 function NavBar(props) {
+
   const myLinks = [
     {link: "/"            , title: "Home"},
     {link: "/projects"    , title: "Projects"},
@@ -16,6 +17,23 @@ function NavBar(props) {
 
   var once = true;
 
+
+  const [showDropdown, setShowDropdown] = useState(false);
+  const navRef = useRef(null);
+
+  const toggleDropdown = () => {
+    setShowDropdown(!showDropdown);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (navRef.current && !navRef.current.contains(event.target)) {
+        setShowDropdown(false);
+      }
+    };
+    document.body.addEventListener('click', handleClickOutside);
+    return () => {document.body.removeEventListener('click', handleClickOutside);};}, []);
+
   return (
     <nav className="navbar" style={props.external ? ({minWidth: 'auto'}) : ({})}>
       <ul>
@@ -23,30 +41,54 @@ function NavBar(props) {
           <div className="tool-bar-title">Bryce Keen</div>
         </li>
 
-        <li style={{marginLeft: 'auto'}}>
+        <li id="nav-link-mobile"  ref={navRef} onClick={toggleDropdown} style={{marginLeft: 'auto'}}>
           <div className="link-container">
-            <FontAwesomeIcon className="mobile-bars" icon={faBars} />
+            <FontAwesomeIcon className="mobile-bars" icon={faBars}/>
           </div>
+
+          <ul className="mobile-dropdown" style={{display: 'block'}}>
+            {myLinks.map((element, i) => 
+              <li id="nav-links"
+                  key={i} 
+                  style={{marginLeft: once ? 'auto' : '', 
+                          display: showDropdown ? 'block' : 'none'}}> 
+
+                {once = false}
+                {props.external ? 
+                (
+                    <a href={element.link} style={{textDecoration: 'none'}}>
+                      <div className="link-container" style={{marginLeft: 'auto'}}>{element.title}</div>
+                    </a>
+                ) : 
+                (
+                    <Link to={element.link} style={{textDecoration: 'none'}}>
+                      <div className="link-container">{element.title}</div>
+                    </Link>
+                )}
+              </li>
+            )}
+          </ ul>
         </li>
 
         {/* Using myLinks generate all the links on the tool bar */}
-        {myLinks.map((element, i) => 
-          <li key={i} style={once ? ({marginLeft: 'auto'}):({})}>  {/* Only give the margin to the first element*/}
-            {once = false}
-            {props.external ? 
-            (
-                <a href={element.link} style={{textDecoration: 'none'}}>
-                  <div className="link-container">{element.title}</div>
-                </a>
-            ) : 
-            (
-                <Link to={element.link} style={{textDecoration: 'none'}}>
-                  <div className="link-container">{element.title}</div>
-                </Link>
-            )}
-          </li>
-        )}
-
+        <div id="nav-links-container" style={{marginLeft: 'auto'}}>
+          {myLinks.map((element, i) => 
+            <li id="nav-links" key={i} style={once ? ({marginLeft: 'auto'}):({})}>  {/* Only give the margin to the first element*/}
+              {once = false}
+              {props.external ? 
+              (
+                  <a href={element.link} style={{textDecoration: 'none'}}>
+                    <div className="link-container">{element.title}</div>
+                  </a>
+              ) : 
+              (
+                  <Link to={element.link} style={{textDecoration: 'none'}}>
+                    <div className="link-container">{element.title}</div>
+                  </Link>
+              )}
+            </li>
+          )}
+        </div>
       </ul>
     </nav>
   );
