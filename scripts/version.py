db595f6c9c257353987e09167a6ad43ad22b5663
PebPeb <67885698+PebPeb@users.noreply.github.com>
2023-12-08 23:31:36 -0500

    Fixed issues with config

--- scripts/version.py
+++ scripts/version.py
@@ -1,16 +1,21 @@
 import json
-import configparser
 
-config = configparser.ConfigParser()
-config.read('./../config.properties')
+properties = {}
+with open('./../config.properties', 'r') as file:
+    for line in file:
+        # Skip comments and empty lines
+        if line.strip() and not line.startswith('#') and not line.startswith(';'):
+            key, value = line.strip().split('=')
+            properties[key.strip()] = value.strip()
+
 
 with open('./../package.json', 'r') as file:
     setup_content = file.read()
 
 setup_data = json.loads(setup_content)
 
-setup_data['version'] = config.get('DEFAULT', 'VERSION')
-setup_data['name'] = config.get('DEFAULT', 'NAME')
+setup_data['version'] = properties.get('VERSION')
+setup_data['name'] = properties.get('NAME')
 
 with open('./../package.json', 'w') as file:
     json.dump(setup_data, file, indent=2)
