4407a18732c920516da23cbacb5f2fdb1a8a9fcd
PebPeb <bmkeen@liberty.edu>
2025-02-26 19:47:43 -0500

    removed NavBar

--- src/react-components/pages/ContactPage/ContactPage.js
+++ src/react-components/pages/ContactPage/ContactPage.js
@@ -3,12 +3,9 @@ import React from "react";
 import "./ContactPage.css";
 import "../GeneralStyle.css";
 
-import NavBar from '../../components/NavBar/NavBar';
-
 function ContactPage() {
   return (
     <div style={{ backgroundColor: '#efefef', minHeight: '100vh' }}>
-      <NavBar />
       <div className="contact-content">
         <h1>Contact Me</h1>
         <p style={{display: "none"}}> Give me a ring ring on the tellie tellie </p>
